8b78190935b66ed79b1de63a0d0e1b434f2dca8f
Keith Valin <kvalin@redhat.com>
2026-02-12 10:40:59 -0500

    Add argument parsing

--- src/sucks/main.py
+++ src/sucks/main.py
@@ -1,2 +1,10 @@
+import argparse
+import logging
+
 def main():
-    print("Hello world!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("container_yaml_file", type=argparse.FileType('r'))
+    parser.add_argument("action", type=str) #TODO make this a dynamic import to the other bits
+    args, unknownargs = parser.parse_known_args()
+    print(args)
+    print(unknownargs)
