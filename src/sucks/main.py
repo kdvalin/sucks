9e8e70148f5a11b85dd6d346e4d8553067ca330f
Keith Valin <kvalin@redhat.com>
2026-02-12 10:48:09 -0500

    Add version command

--- src/sucks/main.py
+++ src/sucks/main.py
@@ -1,10 +1,13 @@
 import argparse
 import logging
 
+from . import __version__
+
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog=__package__)
     parser.add_argument("container_yaml_file", type=argparse.FileType('r'))
     parser.add_argument("action", type=str) #TODO make this a dynamic import to the other bits
+    parser.add_argument("-v", "--version", action="version", version=__version__)
     args, unknownargs = parser.parse_known_args()
     print(args)
     print(unknownargs)
