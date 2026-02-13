21e12de266eb486f3afcbd2d9c720a7054391562
Keith Valin <kvalin@redhat.com>
2026-02-13 14:46:46 -0500

    Add run command

--- src/sucks/commands/__init__.py
+++ src/sucks/commands/__init__.py
@@ -4,12 +4,13 @@ from typing import List, Dict
 from ._base import Command, COMMANDS
 from .setup import Setup
 from .destroy import Destroy
+from .run import RunCommand
 
 COMMANDS: Dict[str, Command] = {}
 
 
 def setup_commands(subparser: argparse._SubParsersAction):
-    _commands_to_setup: List[Command] = [Setup(), Destroy()]
+    _commands_to_setup: List[Command] = [Setup(), Destroy(), RunCommand()]
 
     for i in _commands_to_setup:
         COMMANDS[i._command] = i
---
+++ src/sucks/commands/run.py
@@ -0,0 +1,36 @@
+import argparse
+import podman
+import logging
+
+from ._base import Command
+from sucks.models import ContainerDefinition
+
+logger = logging.getLogger("sucks")
+
+class RunCommand(Command):
+    _command = "run"
+
+    def cli_opts(self, subparser: argparse._SubParsersAction):
+        parser: argparse.ArgumentParser = subparser.add_parser(self._command)
+
+        parser.add_argument("-w", "--workdir", help="Set working dir the command is run in")
+        parser.add_argument("-e", "--env", help="Set enviornment variables", action="append", default=[])
+        parser.add_argument("-i", "--interactive", help="Pass stdin to the command", action="store_true", default=False)
+        parser.add_argument("-t", "--tty", help="Allocate a pseudo-TTY", action="store_true", default=False)
+        parser.add_argument("exec_command", nargs="+", help="The command to run within the container")
+
+    def run_command(self, args: argparse.Namespace, client: podman.PodmanClient):
+        container_def: ContainerDefinition = args.container
+        container_name = f"sucks-{container_def.filename}"
+
+        if not client.containers.exists(container_name):
+            logger.critical(f"No container named {container_name} exists")
+            exit(1)
+        
+        logger.info(f"Running \"{" ".join(args.exec_command)}\" in {container_name}")
+        rtc, output = client.containers.get(container_name).exec_run(
+            args.exec_command
+        )
+
+        print(output.decode('utf-8'), end="")
+        exit(rtc)
