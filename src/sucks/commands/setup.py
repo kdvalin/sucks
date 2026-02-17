2cd720ede819fbf3865eac68c5243585d5949acf
Keith Valin <kvalin@redhat.com>
2026-02-17 14:23:18 -0500

    Move startup commands to ContainerManager

--- src/sucks/commands/setup.py
+++ src/sucks/commands/setup.py
@@ -10,28 +10,6 @@ from typing import List, Dict
 class Setup(Command):
     _command = "setup"
 
-    def _parse_volume_strs(self, vols: List[str]) -> Dict[str, object]:
-        output = {}
-
-        for vol in vols:
-            vol_args = vol.split(':')
-
-            if len(vol_args) < 2 or len(vol_args) > 3:
-                self._logger.warning(f"Volume string {vol} does not match expected format, skipping")
-                continue
-            
-            host_dir = vol_args[0]
-            container_dir = vol_args[1]
-            extended_opts = []
-            if len(vol_args) == 3:
-                extended_opts = vol_args[2].split(',')
-
-            output[host_dir] = {
-                'bind': container_dir,
-                'extended_mode': extended_opts,
-            }
-        return output            
-
     def cli_opts(self, subparser: argparse._SubParsersAction):
         parser = subparser.add_parser(self._command)
         SetupArgs.add_args(parser)
@@ -41,23 +19,16 @@ class Setup(Command):
         if args.conman.exists():
             self._logger.error(f"Container {args.container.container_name} already exists")
             exit(1)
-        
-        try:
-            client.images.pull(args.container.image)
-        except podman.errors.APIError as e:
+
+        if not args.conman.pull():
             self._logger.critical(f"Could not pull {args.container.image}")
-            self._logger.critical(e)
             exit(1)
-        
-        try:
-            client.containers.create(
-                args.container.image,
-                name=args.container.container_name,
-                auto_remove=True,
-                privileged=args.privileged,
-                volumes=self._parse_volume_strs(args.volume)
-            ).start()
-        except podman.errors.APIError as e:
+
+        create_result = args.conman.create(
+            privileged=args.privileged,
+            volumes=args.volume
+        )
+
+        if not create_result:
             self._logger.critical(f"Could not start contianer {args.container.container_name}")
-            self._logger.critical(e)
             exit(1)
--- src/sucks/utils/container.py
+++ src/sucks/utils/container.py
@@ -1,17 +1,45 @@
 import podman
+import logging
+
+from podman.domain.containers import Container
 from sucks.models import ContainerDefinition
+from typing import Dict, List
 
 class ContainerManger:
     _defintion: ContainerDefinition
     _client: podman.PodmanClient
+    _logger: logging.Logger
+
+    @property
+    def _container(self) -> Container:
+        return self._client.containers.get(self._defintion.container_name)
+
+    def _parse_volume_strs(self, vols: List[str]) -> Dict[str, object]:
+        output = {}
+
+        for vol in vols:
+            vol_args = vol.split(':')
+
+            if len(vol_args) < 2 or len(vol_args) > 3:
+                self._logger.warning(f"Volume string {vol} does not match expected format, skipping")
+                continue
+            
+            host_dir = vol_args[0]
+            container_dir = vol_args[1]
+            extended_opts = []
+            if len(vol_args) == 3:
+                extended_opts = vol_args[2].split(',')
+
+            output[host_dir] = {
+                'bind': container_dir,
+                'extended_mode': extended_opts,
+            }
+        return output
 
     def __init__(self, container_def: ContainerDefinition, client: podman.PodmanClient):
         self._defintion = container_def
         self._client = client
-
-    @property
-    def _container(self) -> podman.domain.containers.Container:
-        return self._client.containers.get(self._defintion.container_name)
+        self._logger = logging.getLogger("sucks")
 
     def exists(self) -> bool:
         return self._client.containers.exists(self._defintion.container_name)
@@ -20,5 +48,27 @@ class ContainerManger:
         try:
             self._container.kill()
         except podman.errors.APIError as e:
+            return False
+        return True
+
+    def pull(self) -> bool:
+        try:
+            self._client.images.pull(self._defintion.image)
+        except podman.errors.APIError as e:
+            self._logger.info(e)
+            return False
+        return True
+
+    def create(self, privileged: bool = False, volumes: List[str] = []) -> bool:
+        try:
+            self._client.containers.create(
+                self._defintion.image,
+                name=self._defintion.container_name,
+                auto_remove=True,
+                privileged=privileged,
+                volumes=self._parse_volume_strs(volumes)
+            ).start()
+        except podman.errors.APIError as e:
+            self._logger.info(e)
             return False
         return True
