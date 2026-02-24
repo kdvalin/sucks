cf0fae4663c470548034f1bcf41df866cdc0a5e4
Keith Valin <kvalin@redhat.com>
2026-02-24 15:08:08 -0500

    test(setup): Some expanded testing

--- tests/commands/test_setup.py
+++ tests/commands/test_setup.py
@@ -15,7 +15,13 @@ def conman():
     return MagicMock()
 
 class TestSetup:
-    def test_bare_setup(self, conman, container):
+    @pytest.mark.parametrize('privs,vols', [
+        (True, ["a","b","c"]),
+        (False, ["a","b","c"]),
+        (True, []),
+        (False, [])
+    ])
+    def test_bare_setup(self, conman, container, privs, vols):
         conman.exists = MagicMock(return_value=False)
         conman.pull = MagicMock(return_value=True)
         conman.create = MagicMock(return_value=True)
@@ -25,15 +31,30 @@ class TestSetup:
         setup_args = types.SimpleNamespace(
             conman=conman,
             pull="missing",
-            privileged=False,
-            volume=[],
+            privileged=privs,
+            volume=vols,
             container=container
         )
         Setup().run_command(setup_args, None)
 
         assert conman.exists.called
         conman.pull.assert_called_once_with("missing")
-        conman.create.assert_called_once_with(privileged=False, volumes=[])
+        conman.create.assert_called_once_with(privileged=privs, volumes=vols)
+
+    def test_container_exists(self, conman, container):
+        conman.exists = MagicMock(return_value=True)
+        container.container_name = "ubuntu"
+        setup_args = types.SimpleNamespace(
+            conman=conman,
+            pull="missing",
+            privileged=False,
+            volume=[],
+            container=container
+        )
+        with pytest.raises(SucksException):
+            Setup().run_command(setup_args, None)
+
+        assert conman.exists.called
 
     def test_setup_failed_pull(self, conman, container):
         conman.exists = MagicMock(return_value=False)
