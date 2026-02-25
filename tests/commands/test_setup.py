309366574e33333f40f9e7bc927b32cb8ccb195b
Keith Valin <kvalin@redhat.com>
2026-02-25 12:04:07 -0500

    Wrap up tests for setup command

--- tests/commands/test_setup.py
+++ tests/commands/test_setup.py
@@ -1,6 +1,7 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, call
 import types
 import pytest
+import shlex
 
 from sucks.models.container_file import ContainerDefinition
 from sucks.utils import ContainerManager, SucksException
@@ -73,3 +74,85 @@ class TestSetup:
             Setup().run_command(setup_args, None)
         assert conman.exists.called
         conman.pull.assert_called_once_with("missing")
+
+    def test_failed_create(self, conman, container):
+        conman.exists = MagicMock(return_value=False)
+        conman.pull = MagicMock(return_value=True)
+        conman.create = MagicMock(return_value=False)
+
+        setup_args = types.SimpleNamespace(
+            conman=conman,
+            pull="missing",
+            privileged=False,
+            volume=[],
+            container=container
+        )
+
+        with pytest.raises(SucksException):
+            Setup().run_command(setup_args, None)
+
+        assert conman.exists.called
+        conman.pull.assert_called_once_with("missing")
+        conman.create.assert_called_once_with(privileged=False, volumes=[])
+
+
+    def test_failed_init_start(self, conman, container):
+        conman.exists = MagicMock(return_value=False)
+        conman.pull = MagicMock(return_value=True)
+        conman.create = MagicMock(return_value=True)
+        conman.exec = MagicMock(return_value=1)
+
+        container.initSteps = ["echo hello"]
+
+        setup_args = types.SimpleNamespace(
+            conman=conman,
+            pull="missing",
+            privileged=False,
+            volume=[],
+            container=container
+        )
+
+        with pytest.raises(SucksException):
+            Setup().run_command(setup_args, None)
+
+        assert conman.exists.called
+        conman.pull.assert_called_once_with("missing")
+        conman.create.assert_called_once_with(privileged=False, volumes=[])
+        conman.exec.assert_called_once_with(shlex.split(container.initSteps[0]))
+
+    @pytest.mark.parametrize('initSteps', [
+        (["echo hi", "boo"]),
+        (["one", "two", "three"])
+    ])
+    def test_failed_init_end(self, conman, container, initSteps):
+        conman.exists = MagicMock(return_value=False)
+        conman.pull = MagicMock(return_value=True)
+        conman.create = MagicMock(return_value=True)
+        def exec_side(x):
+            print(x)
+            if x != initSteps[-1]:
+                return 0
+            return 1
+
+        conman.exec = MagicMock(side_effect=exec_side)
+        
+
+        container.initSteps = initSteps
+
+        setup_args = types.SimpleNamespace(
+            conman=conman,
+            pull="missing",
+            privileged=False,
+            volume=[],
+            container=container
+        )
+
+        with pytest.raises(SucksException):
+            with patch('shlex.split') as mock_split:
+                mock_split.side_effect = lambda x: x
+                Setup().run_command(setup_args, None)
+
+        assert conman.exists.called
+        conman.pull.assert_called_once_with("missing")
+        conman.create.assert_called_once_with(privileged=False, volumes=[])
+        assert conman.exec.mock_calls == [call(i) for i in initSteps]
